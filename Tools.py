3a2d1567f70156c67b441c43a46a76dc2fe901d4
erikvanlennep <ewr.lennep@portofrotterdam.com>
2025-11-04 12:19:55 +0100

    opruimen functies

--- Tools.py
+++ Tools.py
@@ -1,31 +1,52 @@
 from langchain_core.tools import tool
-from langchain_openai import OpenAI
+from langchain_openai import OpenAI as OpenAILangchain
+from langchain_community.document_loaders import UnstructuredPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
+from openai import OpenAI
 
-@tool
+def load_pdf(file_path):
+    """Inladen van de PDF"""
+    loader = UnstructuredPDFLoader(file_path)  # <-- jouw bestand
+    return loader.load()
+
+def text_to_chunks(documents, size=1000, overlap=200):
+    """split de tekst van de pdf in chunks"""
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=size,  # aantal tekens per chunk
+        chunk_overlap=overlap,  # overlap om context te behouden
+        separators=["\n\n", "\n", ".", " ", ""],
+    )
+
+    return splitter.split_documents(documents)
+
 def embed_chunks(chunks):
+    """Query de database"""
     client = OpenAI()
-
     embedded_chunks = []
 
-    for chunk in chunks:
-        embedding = client.embeddings.create(
-            model="text-embedding-3-small",
-            input=chunk
-        ).data[0].embedding
+    # Filter lege chunks
+    texts = [c.page_content for c in chunks if c.page_content.strip()]
 
-        embedded_chunks.append(embedding)
+    # In batches van 100 (API ondersteunt tot 2048 inputs per call)
+    batch_size = 100
+    for batch in range(0, len(texts), batch_size):
+        batch = texts[batch:batch + batch_size]
+        response = client.embeddings.create(
+            model="text-embedding-3-small",
+            input=batch
+        )
+        for e in response.data:
+            embedded_chunks.append(e.embedding)
 
     return embedded_chunks
 
-
-@tool
 def query_db(vectors):
-    client = OpenAI()
+    client = OpenAILangchain()
 
     embedded_chunks = []
 
-    for chunk in chunks:
+    for chunk in vectors: # todo: change
         embedding = client.embeddings.create(
             model="text-embedding-3-small",
             input=chunk
--- main.py
+++ main.py
@@ -1,48 +1,7 @@
-from langchain_openai import ChatOpenAI
-from langchain.agents import create_agent
-from langchain_core.tools import tool
-from langchain_community.document_loaders import UnstructuredPDFLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
 from dotenv import load_dotenv
-
-from openai import OpenAI
-
-def load_pdf(file_path):
-    loader = UnstructuredPDFLoader(file_path)  # <-- jouw bestand
-    return loader.load()
-
-def text_to_chunks(documents, size=1000, overlap=200):
-    splitter = RecursiveCharacterTextSplitter(
-        chunk_size=size,  # aantal tekens per chunk
-        chunk_overlap=200,  # overlap om context te behouden
-        separators=["\n\n", "\n", ".", " ", ""],
-    )
-
-    return splitter.split_documents(documents)
-
-def embed_chunks(chunks):
-    client = OpenAI()
-    embedded_chunks = []
-
-    # Filter lege chunks
-    texts = [c.page_content for c in chunks if c.page_content.strip()]
-
-    # In batches van 100 (API ondersteunt tot 2048 inputs per call)
-    batch_size = 100
-    for batch in range(0, len(texts), batch_size):
-        batch = texts[batch:batch + batch_size]
-        response = client.embeddings.create(
-            model="text-embedding-3-small",
-            input=batch
-        )
-        for e in response.data:
-            embedded_chunks.append(e.embedding)
-
-    return embedded_chunks
-
+from Tools import load_pdf, text_to_chunks, embed_chunks
 
 # -- Main function --
-
 if __name__ == "__main__":
     load_dotenv()
 
